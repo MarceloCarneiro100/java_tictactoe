17bf08648558fb0843a426efa3a30cebc225005d
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-02 13:36:18 -0300

    Partial Board class implementation

--- src/br/com/project/tictactoe/core/Board.java
+++ src/br/com/project/tictactoe/core/Board.java
@@ -1,24 +1,64 @@
 package br.com.project.tictactoe.core;
 
 import br.com.project.tictactoe.Constants;
+import br.com.project.tictactoe.ui.UI;
 
 public class Board {
 
 	char[][] matrix = new char[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 
-	void clear() {
-
+	public Board() {
+		clear();
 	}
 
-	void print() {
-
+	public void clear() {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = ' ';
+			}
+		}
 	}
 
-	boolean isFull() {
+	public void print() {
+
+		UI.printNewLine();
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+
+				UI.printTextWithNoNewLine(String.valueOf(matrix[i][j]));
+
+				if (j < matrix[i].length - 1) {
+					UI.printTextWithNoNewLine(" | ");
+				}
+			}
+
+			UI.printNewLine();
+
+			if (i < matrix.length - 1) {
+				UI.printText("---------");
+			}
+		}
+	}
+
+	public boolean isFull() {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (matrix[i][j] == ' ') {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean play(Player player, Move move) {
+		int i = move.i;
+		int j = move.j;
+
+		matrix[i][j] = player.symbol;
+
+		// TODO Check if player has won
 		return false;
-	}
-
-	void play() {
-
 	}
 }
--- src/br/com/project/tictactoe/core/Game.java
+++ src/br/com/project/tictactoe/core/Game.java
@@ -10,6 +10,8 @@ public class Game {
 
 	public void play() {
 		UI.printGameTitle();
-		UI.readInput("Nome do Jogador: ");
+		board.print();
+		
+		// UI.readInput("Nome do Jogador: ");
 	}
 }

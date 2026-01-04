0935bf78dad4ceaea6f3dedb9d58350f15f411d4
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-03 21:07:02 -0300

    Add initial logic to play method in class Game

--- src/br/com/project/tictactoe/core/Game.java
+++ src/br/com/project/tictactoe/core/Game.java
@@ -10,8 +10,19 @@ public class Game {
 
 	public void play() {
 		UI.printGameTitle();
-		board.print();
-		
-		// UI.readInput("Nome do Jogador: ");
+
+		for (int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+		}
+	}
+
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + " => ");
+		char symbol = Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
+
+		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'");
+
+		return player;
 	}
 }
